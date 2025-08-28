1b47ca9e908a89c462c6981650eac052c929f044
jackaberdeen15 <43541345+jackaberdeen15@users.noreply.github.com>
2025-08-28 16:23:06 +0100

    Update LiDAR_Sim.py
    implemented first stage of photon time stamp generation. Next step is to implement dead time modelling.

--- LiDAR_Sim.py
+++ LiDAR_Sim.py
@@ -11,7 +11,11 @@ Model being implemented is based on the methodology outlined in https://doi.org/
 #import scipy
 import json
 import numpy as np
+import scipy
+import matplotlib.pyplot as plt
 
+
+dt = 50e-12
 
 def generate_scene_conditions(data, batch_size):
     
@@ -72,9 +76,56 @@ def photon_numbers_scene(data, R, ref, SPAD_dist, ambient):
     
     return np.where(mask,Psig,0.0), np.where(mask, Pnoise, 0.0)
 
-def photon_numbers_batch(data, R_list, ref_list, SPAD_list, ambient):
+def photon_numbers_batch(data, R_batch, ref_batch, SPAD_dist_batch, ambient):
     return zip(*[photon_numbers_scene(data, R, ref, SPAD_dist, ambient)
-                 for R, ref, SPAD_dist in zip(R_list, ref_list, SPAD_list)])
+                 for R, ref, SPAD_dist in zip(R_batch, ref_batch, SPAD_dist_batch)])
+
+def expand_to_SPADs(data,Psig, Pnoise, R, SPAD_dists):
+    n_spads = data["SPADnum"]
+    Psig_expand = np.zeros((len(Psig), n_spads))
+    Pnoise_expand = np.zeros((len(Pnoise), n_spads))
+    R_expand = np.zeros((len(R), n_spads))
+    
+    for i in range(len(Psig)):
+        Psig_expand[i] = np.repeat(Psig[i], SPAD_dists[i])
+        Pnoise_expand[i] = np.repeat(Pnoise[i], SPAD_dists[i])
+        R_expand[i] = np.repeat(R[i], SPAD_dists[i])
+
+    return Psig_expand, Pnoise_expand, R_expand
+
+def timestamp_generation_surface(data, Psig, Pnoise, SPAD_dist, pdf):
+    c = 3e8 # speed of light in m/s
+    Laser_cycles = data["Laser_cycles"]
+    Tpulse = data["Tpulse"]
+    Bin_num = data["Bin_num"]
+    Bin_width = data["Bin_width"]
+    N_SPADs = data["SPADnum"]
+    
+    Hist = np.zeros((Bin_num,), dtype=np.uint16)
+    
+    sig_tstmp_buffer = np.zeros((N_SPADs,int(Tpulse / dt)))
+    amb_tstmp_buffer = np.zeros((N_SPADs,int(Bin_num*Bin_width / dt)))
+    
+    # generate timestamps per laser cycle, then allocate to histogram which is kept between laser cycles as in
+    # real systems
+    # generate timestamps (up to 100 per laser cycle for both sig and noise, more is not needed?) from these
+    # timesteps, the histogram can be incremented
+    
+    for i in range(N_SPADs):
+        rate = pdf * Psig[i]
+        
+        for j in range(len(rate)-1):
+            bin_rate = (rate[j] + rate[j+1]) / 2 #interpolate between bin edges
+            sig_tstmp_buffer[i,j*10:j*10+10] = np.random.poisson(bin_rate,(10,))
+            
+
+
+def timestamp_generation_batch(data, Psig_batch, Pnoise_batch, SPAD_dist_batch):
+    samples = np.linspace(-data["Tpulse"],data["Tpulse"], int(data["Tpulse"] / dt / 10 + 1))
+    pdf = scipy.stats.norm.pdf(samples, 0, data["Tpulse"]/2.355)
+    
+    # normalise pdf
+    pdf /= np.max(pdf)
 
 #Load SPAD-based LiDAR parameters from .json
 with open("LiDAR_params.json","r") as file:
@@ -82,7 +133,64 @@ with open("LiDAR_params.json","r") as file:
 
 R, ref, SPAD_dist = generate_scene_conditions(data,10)
 
-Psig, Pnoise = photon_numbers_batch(data,R,ref,SPAD_dist,20e3)
+Psig, Pnoise = photon_numbers_batch(data,R,ref, SPAD_dist, 20e3)
 
-print(Psig)
-print(Pnoise)
+Psig_s, Pnoise_s, R_s = expand_to_SPADs(data, Psig, Pnoise, R, SPAD_dist)
+
+
+#print(Psig)
+#print(Pnoise)
+
+#%%
+
+samples = np.linspace(-data["Tpulse"] / dt ,data["Tpulse"] / dt, int(data["Tpulse"] / dt / 10 + 1))
+dist = scipy.stats.norm(0, data["Tpulse"] / dt /2.355)
+pdf = dist.pdf(samples)
+
+# normalise pdf
+pdf /= np.sum(pdf)
+
+c = 3e8 # speed of light in m/s
+Laser_cycles = data["Laser_cycles"]
+Tpulse = data["Tpulse"]
+Bin_num = data["Bin_num"]
+Bin_width = data["Bin_width"]
+N_SPADs = data["SPADnum"]
+cycle_len = int(Bin_num*Bin_width / dt)
+Tdead = int(data["Tdead"]/dt)
+
+Hist = np.zeros((Bin_num,), dtype=np.uint16)
+
+sig_tstmp_buffer = np.zeros((N_SPADs,int(Tpulse / dt)))
+tstmp_buffer = np.zeros((N_SPADs,cycle_len))
+tstmp_buffer_merged = np.zeros((cycle_len,))
+
+# generate timestamps per laser cycle, then allocate to histogram which is kept between laser cycles as in
+# real systems
+# generate timestamps (up to 100 per laser cycle for both sig and noise, more is not needed?) from these
+# timesteps, the histogram can be incremented
+
+sel = 0
+
+for i in range(N_SPADs):
+    rate = pdf * Psig_s[sel,i]
+    
+    for j in range(len(rate)-1):
+        bin_rate = (rate[j] + rate[j+1]) / 2 #interpolate between bin edges
+        sig_tstmp_buffer[i,j*10:j*10+10] = np.random.poisson(bin_rate/10,(10,))
+    
+    # add ambient
+    tstmp_buffer[i] = np.random.poisson(Pnoise_s[sel,i]/cycle_len, cycle_len)
+    
+    surface_index = int(R_s[sel,i] *2 / c / dt)
+    
+    tstmp_buffer[i,surface_index:surface_index+sig_tstmp_buffer.shape[-1]] = sig_tstmp_buffer[i]
+
+tstmp_buffer_merged = np.sum(tstmp_buffer,0)
+
+print(R[sel])
+print(ref[sel])
+print(np.sum(Psig[sel]))
+print(np.sum(sig_tstmp_buffer))
+print(np.sum(Pnoise[sel]))
+print(np.sum(tstmp_buffer))
